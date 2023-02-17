9736f656bf1c420a2eaf8c629bd8c05735fe450c
L3odr0id <leon.leonc@yandex.ru>
2023-02-17 08:58:15 +0000

    Add leaders and fields

--- values/leaders.py
+++ values/leaders.py
@@ -3,7 +3,7 @@ from storage.leaders_storage import LeadersStorage
 from data.leader import Leader
 
 # Лидеры
-_Alexandr_Makedonia = Leader('Александр', 'Македония', 0)
+_Alexandr_Makedonia = Leader('Александр', 'Македония', 0, is_banned=True)
 _Alieanora_England = Leader('Алиенора Аквитанская (Англия)', 'Англия', 1)
 _Alieanora_France = Leader('Алиенора Аквитанская (Франция)', 'Франция', 2)
 _Amanitore = Leader('Аманиторе', 'Нубия', 3)
@@ -15,14 +15,13 @@ _Gandi = Leader('Ганди', 'Индия', 8)
 _Gilgamesh = Leader('Гильгамеш', 'Шумер', 9)
 _Gorgo_Grece = Leader('Горго', 'Греция', 10)
 _Javarman = Leader('Джайаварман VII', 'Кхмеры', 11)
-_Jhon_Cartin = Leader('Джон Кэртин', 'Австралия', 12)
+_Jhon_Cartin = Leader('Джон Кэртин', 'Австралия', 12, is_banned=True)
 _Didona = Leader('Дидона', 'Финикия', 13)
 _Ekatetina_Medichi_Velikolepnaya = Leader('Екатерина Медичи (Великолепная)', 'Франция', 14)
 _Juan_III = Leader('Жуан III', 'Португалия', 15)
 _Ish_vak = Leader('Иш-Вак-Чан-Ахав', 'Майя', 16)
 _Kir = Leader('Кир II', 'Персия', 17)
 _Cleopatra = Leader('Клеопатра', 'Египет', 18)
-_Konstantin_Ostrog = Leader('Константин Острожский', 'Волынь', 19)
 _Kristina_I = Leader('Кристина', 'Швеция', 20)
 _Kupe = Leader('Купе', 'Маори', 21)
 _Lautaro = Leader('Лаутаро', 'Мапуче', 22)
@@ -37,10 +36,10 @@ _Pochatok = Leader('Пачакутек', 'Инки', 30)
 _Pedru_II = Leader('Педру II', 'Бразилия', 31)
 _Perikl = Leader('Перикл', 'Греция', 32)
 _Robert_Brus = Leader('Роберт Брюс', 'Шотландия', 33)
-_Saladin = Leader('Саладин', 'Аравия', 34)
+_Saladin = Leader('Саладин (Визирь)', 'Аравия', 34)
 _Simon_Bolivar = Leader('Симон Боливар', 'Колумбия', 35)
-_Sondok = Leader('Сондок', 'Корея', 36)
-_Suleiman = Leader('Сулейман', 'Османы', 37)
+_Sondok = Leader('Сондок', 'Корея', 36, is_banned=True)
+_Suleiman = Leader('Сулейман (Кануни)', 'Османы', 37)
 _Tamara = Leader('Тамара', 'Грузия', 38)
 _Rusvelt_Progressive = Leader('Теодор Рузвельт (Прогрессивист)', 'США', 39)
 _Rusvelt_Vsadnik = Leader('Теодор Рузвельт (Мужественный всадник)', 'США', 40)
@@ -50,12 +49,12 @@ _Tribuhvana = Leader('Трибхувана', 'Индонезия', 43)
 _Wilfried_Lorie = Leader('Уилфрид Лорье', 'Канада', 44)
 _Filipp_2 = Leader('Филипп II', 'Испания', 45)
 _Friedrich_Barbarossa = Leader('Фридрих Барбаросса', 'Германия', 46)
-_Hammurapi = Leader('Хаммурапи', 'Вавилон', 47)
+_Hammurapi = Leader('Хаммурапи', 'Вавилон', 47, is_banned=True)
 _Harald = Leader('Харальд Суровый', 'Норвегия', 48)
 _Hojo_Tokimune = Leader('Ходзё Токимунэ', 'Япония', 49)
 _Hubilai_Mongol = Leader('Хубилай (Монголия)', 'Монголия', 50)
 _Hubilau_China = Leader('Хубилай (Китай)', 'Китай', 51)
-_Tin_Huan = Leader('Цинь Шихуанди', 'Китай', 52)
+_Tin_Huan = Leader('Цинь Шихуанди (Небесный Мандат)', 'Китай', 52)
 _Chaka = Leader('Чака', 'Зулусы', 53)
 _Chandaragupta = Leader('Чандрагупта', 'Индия', 54)
 _Chingishan = Leader('Чингисхан', 'Монголия', 55)
@@ -64,6 +63,15 @@ _Yadviga = Leader('Ядвига', 'Польша', 57)
 _Ekaterina_Medichi_Black_Queen = Leader('Екатерина Медичи (Чёрная королева)', 'Франция', 58)
 _Wu_Zeitian = Leader('У Цзетянь', 'Китай', 59)
 _Tokugawa = Leader('Токугава', 'Япония', 60)
+_Suleiman_muhtesem = Leader('Сулейман (Muhteşem)', 'Османы', 61)
+_Abraham_Linkoln = Leader('Авраам Линкольн', 'США', 62, is_banned=True)
+_Nader_shah = Leader('Надер Шах', 'Персия', 63)
+_Nzinga_Mbandi = Leader('Нзинга Мбанди', 'Конго', 64)
+_Cesar = Leader('Юлий Цезарь', 'Рим', 65, is_banned=True)
+_Tin_Huan_unifier = Leader('Цинь Шихуанди (Освободитель)', 'Китай', 66)
+_Saladin_sultan = Leader('Саладин (Султан)', 'Аравия', 67)
+_Yongle = Leader('Юнлэ', 'Китай', 68)
+# 19 id свободен, его можно занять. Раньше там стоял константин острожский
 
 
 leadersStorage = LeadersStorage([
@@ -86,7 +94,6 @@ leadersStorage = LeadersStorage([
     _Ish_vak,
     _Kir,
     _Cleopatra,
-    _Konstantin_Ostrog,
     _Kristina_I,
     _Kupe,
     _Lautaro,
@@ -127,5 +134,13 @@ leadersStorage = LeadersStorage([
     _Yadviga,
     _Ekaterina_Medichi_Black_Queen,
     _Wu_Zeitian,
-    _Tokugawa
+    _Tokugawa,
+    _Suleiman_muhtesem,
+    _Abraham_Linkoln,
+    _Nader_shah,
+    _Nzinga_Mbandi,
+    _Cesar,
+    _Tin_Huan_unifier,
+    _Saladin_sultan,
+    _Yongle
 ])
